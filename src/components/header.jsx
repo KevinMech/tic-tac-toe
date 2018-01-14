49574e3de6ef6eaba8aadbcbe35e2dd57673f245
KevinMech <kevin.mellachew@gmail.com>
2018-01-14 11:48:05 -0800

    Add CSS stylesheet

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../style.css';
 
 const Header = () => {
-    return <h1>Tic-Tac-Toe</h1>;
+    return <h1 className="header">Tic-Tac-Toe</h1>;
 }
 
 export default Header;
