669ebd5f44fd9fab3cde7a15c5d6f471cb77ce6c
KevinMech <kevin.mellachew@gmail.com>
2018-01-14 12:05:39 -0800

    Add Board Component and Style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Header from './components/header.jsx'; 
+import Header from './components/header.jsx';
+import Board from './components/board.jsx';
 
 const App = () => {
-	return <Header/>;
+	return (
+        <div>
+            <Header/>
+            <Board/>
+        </div>
+        );
 }
 
 ReactDom.render(<App/>, document.getElementById('root'));
